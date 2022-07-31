e1423efe476972b45c0a60c6124fb916d6f1e1aa
amirhosein <amirhoseinh1373@gmail.com>
2022-07-31 23:28:23 +0430

    register user functions

--- wp-content/themes/store/functions.php
+++ wp-content/themes/store/functions.php
@@ -272,35 +272,229 @@ function jk_woocommerce_breadcrumbs() {
     );
 }
 
-add_action('user_register','my_function');
+function register_new_user( $user_login, $user_email ) {
+	$errors = new WP_Error();
 
-function my_function($user_id){
-  //do your stuff
-  echo $user_id;
+	$sanitized_user_login = sanitize_user( $user_login );
+	/**
+	 * Filters the email address of a user being registered.
+	 *
+	 * @since 2.1.0
+	 *
+	 * @param string $user_email The email address of the new user.
+	 */
+	$user_email = apply_filters( 'user_registration_email', $user_email );
+
+	// Check the username.
+	if ( '' === $sanitized_user_login ) {
+		$errors->add( 'empty_username', __( '<strong>Error</strong>: Please enter a username.' ) );
+	} elseif ( ! validate_username( $user_login ) ) {
+		$errors->add( 'invalid_username', __( '<strong>Error</strong>: This username is invalid because it uses illegal characters. Please enter a valid username.' ) );
+		$sanitized_user_login = '';
+	} elseif ( username_exists( $sanitized_user_login ) ) {
+		$errors->add( 'username_exists', __( '<strong>Error</strong>: This username is already registered. Please choose another one.' ) );
+
+	} else {
+		/** This filter is documented in wp-includes/user.php */
+		$illegal_user_logins = (array) apply_filters( 'illegal_user_logins', array() );
+		if ( in_array( strtolower( $sanitized_user_login ), array_map( 'strtolower', $illegal_user_logins ), true ) ) {
+			$errors->add( 'invalid_username', __( '<strong>Error</strong>: Sorry, that username is not allowed.' ) );
+		}
+	}
+
+	// Check the email address.
+	if ( '' === $user_email ) {
+		$errors->add( 'empty_email', __( '<strong>Error</strong>: Please type your email address.' ) );
+	} elseif ( ! is_email( $user_email ) ) {
+		$errors->add( 'invalid_email', __( '<strong>Error</strong>: The email address is not correct.' ) );
+		$user_email = '';
+	} elseif ( email_exists( $user_email ) ) {
+		$errors->add(
+			'email_exists',
+			sprintf(
+				/* translators: %s: Link to the login page. */
+				__( '<strong>Error:</strong> This email address is already registered. <a href="%s">Log in</a> with this address or choose another one.' ),
+				wp_login_url()
+			)
+		);
+	}
+
+	/**
+	 * Fires when submitting registration form data, before the user is created.
+	 *
+	 * @since 2.1.0
+	 *
+	 * @param string   $sanitized_user_login The submitted username after being sanitized.
+	 * @param string   $user_email           The submitted email.
+	 * @param WP_Error $errors               Contains any errors with submitted username and email,
+	 *                                       e.g., an empty field, an invalid username or email,
+	 *                                       or an existing username or email.
+	 */
+	do_action( 'register_post', $sanitized_user_login, $user_email, $errors );
+
+	/**
+	 * Filters the errors encountered when a new user is being registered.
+	 *
+	 * The filtered WP_Error object may, for example, contain errors for an invalid
+	 * or existing username or email address. A WP_Error object should always be returned,
+	 * but may or may not contain errors.
+	 *
+	 * If any errors are present in $errors, this will abort the user's registration.
+	 *
+	 * @since 2.1.0
+	 *
+	 * @param WP_Error $errors               A WP_Error object containing any errors encountered
+	 *                                       during registration.
+	 * @param string   $sanitized_user_login User's username after it has been sanitized.
+	 * @param string   $user_email           User's email.
+	 */
+	$errors = apply_filters( 'registration_errors', $errors, $sanitized_user_login, $user_email );
+
+	if ( $errors->has_errors() ) {
+		return $errors;
+	}
+
+	$user_pass = wp_generate_password( 12, false );
+	$user_id   = wp_create_user( $sanitized_user_login, $user_pass, $user_email );
+	if ( ! $user_id || is_wp_error( $user_id ) ) {
+		$errors->add(
+			'registerfail',
+			sprintf(
+				/* translators: %s: Admin email address. */
+				__( '<strong>Error</strong>: Could not register you&hellip; please contact the <a href="mailto:%s">site admin</a>!' ),
+				get_option( 'admin_email' )
+			)
+		);
+		return $errors;
+	}
+
+	update_user_meta( $user_id, 'default_password_nag', true ); // Set up the password change nag.
+
+	if ( ! empty( $_COOKIE['wp_lang'] ) ) {
+		$wp_lang = sanitize_text_field( $_COOKIE['wp_lang'] );
+		if ( in_array( $wp_lang, get_available_languages(), true ) ) {
+			update_user_meta( $user_id, 'locale', $wp_lang ); // Set user locale if defined on registration.
+		}
+	}
+
+	/**
+	 * Fires after a new user registration has been recorded.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @param int $user_id ID of the newly registered user.
+	 */
+	do_action( 'register_new_user', $user_id );
+
+	return $user_id;
 }
 
-add_filter('woocommerce_save_account_details_required_fields', 'remove_required_email_address');
-
-function remove_required_email_address( $required_fields ) {
-    unset($required_fields['account_email']);
-
-    return $required_fields;
-}
-
-function custom_override_checkout_fields( $fields ) {
-    unset($fields['billing']['billing_email']);    
-    return $fields;
-}
-add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields', 1000, 1 );
-
-add_action( 'woocommerce_process_registration_errors', 'custom_registration_error' );
-
-function custom_registration_errors( $validation_error ){
-    if ( ! isset( $_POST['phone'] ) ){
-        $validation_error = new WP_Error( 'phone-error', __( 'Please enter a phone number.', 'your-plugin' ) );
+function wc_create_new_customer( $email, $username = '', $password = '', $args = array() ) {
+    if ( empty( $email ) || ! is_email( $email ) ) {
+        return new WP_Error( 'registration-error-invalid-email', __( 'Please provide a valid email address.', 'woocommerce' ) );
     }
-    return $validation_error;
+
+    if ( email_exists( $email ) ) {
+        return new WP_Error( 'registration-error-email-exists', apply_filters( 'woocommerce_registration_error_email_exists', __( 'An account is already registered with your email address. <a href="#" class="showlogin">Please log in.</a>', 'woocommerce' ), $email ) );
+    }
+
+    if ( 'yes' === get_option( 'woocommerce_registration_generate_username', 'yes' ) && empty( $username ) ) {
+        $username = wc_create_new_customer_username( $email, $args );
+    }
+
+    $username = sanitize_user( $username );
+
+    if ( empty( $username ) || ! validate_username( $username ) ) {
+        return new WP_Error( 'registration-error-invalid-username', __( 'Please enter a valid account username.', 'woocommerce' ) );
+    }
+
+    if ( username_exists( $username ) ) {
+        return new WP_Error( 'registration-error-username-exists', __( 'An account is already registered with that username. Please choose another.', 'woocommerce' ) );
+    }
+
+    // Handle password creation.
+    $password_generated = false;
+    if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && empty( $password ) ) {
+        $password           = wp_generate_password();
+        $password_generated = true;
+    }
+
+    if ( empty( $password ) ) {
+        return new WP_Error( 'registration-error-missing-password', __( 'Please enter an account password.', 'woocommerce' ) );
+    }
+
+    // Use WP_Error to handle registration errors.
+    $errors = new WP_Error();
+
+    do_action( 'woocommerce_register_post', $username, $email, $errors );
+
+    $errors = apply_filters( 'woocommerce_registration_errors', $errors, $username, $email );
+
+    if ( $errors->get_error_code() ) {
+        return $errors;
+    }
+
+    $new_customer_data = apply_filters(
+        'woocommerce_new_customer_data',
+        array_merge(
+            $args,
+            array(
+                'user_login' => $username,
+                'user_pass'  => $password,
+                'user_email' => $email,
+                'role'       => 'customer',
+            )
+        )
+    );
+
+    $customer_id = wp_insert_user( $new_customer_data );
+
+    if ( is_wp_error( $customer_id ) ) {
+        return $customer_id;
+    }
+
+    do_action( 'woocommerce_created_customer', $customer_id, $new_customer_data, $password_generated );
+
+    return $customer_id;
 }
+
+// function wpdocs_use_email_as_username( $user_email ) {
+//     var_dump( $user_email );
+//     die;
+//     return $_POST['user_login'];
+// }
+ 
+// add_filter( 'user_registration_email', 'wpdocs_use_email_as_username' );
+
+// add_action('user_register','my_function');
+
+// function my_function($user_id){
+//   //do your stuff
+//   echo $user_id;
+// }
+
+// add_filter('woocommerce_save_account_details_required_fields', 'remove_required_email_address');
+
+// function remove_required_email_address( $required_fields ) {
+//     unset($required_fields['account_email']);
+
+//     return $required_fields;
+// }
+
+// function custom_override_checkout_fields( $fields ) {
+//     unset($fields['billing']['billing_email']);    
+//     return $fields;
+// }
+// add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields', 1000, 1 );
+
+// add_action( 'woocommerce_process_registration_errors', 'custom_registration_error' );
+
+// function custom_registration_errors( $validation_error ){
+//     if ( ! isset( $_POST['phone'] ) ){
+//         $validation_error = new WP_Error( 'phone-error', __( 'Please enter a phone number.', 'your-plugin' ) );
+//     }
+//     return $validation_error;
+// }
 
 //woocommerce_register_post 
 //wc_create_new_customer
